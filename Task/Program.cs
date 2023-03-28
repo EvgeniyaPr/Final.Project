f99edbec45087d49809c854bb7833c7a7bcb615a
Evgeniya <prahova.e@ya.ru>
2023-03-28 15:13:04 +0500

    Added 2 methods: GetArray, PrintArray

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,20 @@
 ﻿// Задача: Написать программу, которая из имеющегося массива строк формирует массив из сторк, длина которых меньше или равна 3 символа. 
 // Первоначальный массив можно ввести с клавиатуры.
+
+string[] GetArray(string text)
+{
+    System.Console.Write(text);
+    var array = Array.ConvertAll(Console.ReadLine().Split(", "), Convert.ToString);
+    return array;
+}
+
+void PrintArray(string[] array)
+{
+    System.Console.WriteLine("[" + string.Join(",", array) + "]");
+}
+
+
+
+
+
+string[] myArray = GetArray("Введите строки, разделяя их запятой: ");
